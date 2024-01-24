2b7cdf95bad03bccaa390a67afb7c67e68f46aa3
Gabriel Martins <martinsgabriel1956@gmail.com>
2024-01-24 16:49:00 -0300

    feat: apply useMemo hook

--- src/providers/TransactionsProvider.tsx
+++ src/providers/TransactionsProvider.tsx
@@ -43,7 +43,7 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
 
   useEffect(() => {
     fetchTransactions()
-  }, [])
+  }, [fetchTransactions])
 
   return (
     <TransactionsContext.Provider
